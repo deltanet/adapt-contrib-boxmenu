a204ec9dbd542ea8f799a589786662d72fcaeed8
RobertPeek <robert@delta-net.co.uk>
2016-01-20 14:20:03 +0000

    Set audio to only play once

--- js/adapt-contrib-boxmenu.js
+++ js/adapt-contrib-boxmenu.js
@@ -5,9 +5,7 @@ define([
 
     var BoxMenuView = MenuView.extend({
 
-        events: {
-            'click .menu-item-audio-toggle': 'toggleAudio'
-        },
+        events: {},
 
         postRender: function() {
             var nthChild = 0;
@@ -18,56 +16,7 @@ define([
                     this.$('.menu-container-inner').append(new BoxMenuItemView({model: item}).$el);
                 }
             });
-
-            this.audioChannel = this.model.get('_audio')._channel;
-            this.elementId = this.model.get("_id");
-
-            // Hide controls
-            if(this.model.get('_audio')._showControls==false){
-                this.$('.audio-toggle').addClass('hidden');
-            }
-            try {
-                this.audioFile = this.model.get("_audio")._media.mp3;
-            } catch(e) {
-                console.log('An error has occured loading audio');
-            }
-
-            // Hide audio icon if config.JSON audio is false
-            if (Adapt.config.get("_audio") && Adapt.config.get("_audio")._isEnabled) {
-                // Do nothing
-            } else {
-                this.$('.menu-item-audio-inner').css('display', 'none');
-            }
-
-            // Set clip ID
-            Adapt.audio.audioClip[this.audioChannel].newID = this.elementId;
-            // Set listener for when clip ends
-            $(Adapt.audio.audioClip[this.audioChannel]).on('ended', _.bind(this.onAudioEnded, this));
-
-            // Check if audio is set to on
-            if(Adapt.audio.audioClip[this.audioChannel].status==1){
-                // Check if audio is set to autoplay
-                if(this.model.get("_audio")._isEnabled && this.model.get("_audio")._autoplay){
-                    Adapt.trigger('audio:playAudio', this.audioFile, this.elementId, this.audioChannel);
-                }
-            }
-
-        },
-
-        onAudioEnded: function() {
-            Adapt.trigger('audio:audioEnded', this.audioChannel);
-        },
-
-        toggleAudio: function(event) {
-            if (event) event.preventDefault();
- 
-            if ($(event.currentTarget).hasClass('playing')) {
-                Adapt.trigger('audio:pauseAudio', this.audioChannel);
-            } else {
-                Adapt.trigger('audio:playAudio', this.audioFile, this.elementId, this.audioChannel);
-            }
         }
-
     }, {
         template: 'boxmenu'
     });
@@ -114,13 +63,76 @@ define([
         template: 'boxmenu-item'
     });
 
-    Adapt.on('router:menu', function(model) {
 
-        $('#wrapper').append(new BoxMenuView({model: model}).$el);
+    var BoxMenuAudioView = Backbone.View.extend({
+
+        className: 'boxmenu-audio',
+
+        initialize: function() {
+            this.render();
+        },
+
+        events: {
+            'click .menu-item-audio-toggle': 'toggleAudio'
+        },
+
+        render: function() {
+            var data = this.model.toJSON();
+            var template = Handlebars.templates["boxmenu-audio"];
+            this.$el.html(template(data)).appendTo('.menu-header-inner');
+
+            this.audioChannel = this.model.get('_audio')._channel;
+            this.elementId = this.model.get("_id");
+
+            // Hide controls
+            if(this.model.get('_audio')._showControls==false){
+                this.$('.audio-toggle').addClass('hidden');
+            }
+            try {
+                this.audioFile = this.model.get("_audio")._media.mp3;
+            } catch(e) {
+                console.log('An error has occured loading audio');
+            }
+            // Hide audio icon if config.JSON audio is false
+            if (Adapt.config.get("_audio") && Adapt.config.get("_audio")._isEnabled) {
+                // Do nothing
+            } else {
+                this.$('.menu-item-audio-inner').css('display', 'none');
+            }
+            // Set clip ID
+            Adapt.audio.audioClip[this.audioChannel].newID = this.elementId;
+            // Set listener for when clip ends
+            $(Adapt.audio.audioClip[this.audioChannel]).on('ended', _.bind(this.onAudioEnded, this));
+
+            // Check if audio is set to on
+            if(Adapt.audio.audioClip[this.audioChannel].status==1){
+                // Check if audio is set to autoplay
+                if(this.model.get("_audio")._isEnabled && this.model.get("_audio")._autoplay){
+                    Adapt.trigger('audio:playAudio', this.audioFile, this.elementId, this.audioChannel);
+                }
+            }
+            return this;
+        },
+
+        onAudioEnded: function() {
+            Adapt.trigger('audio:audioEnded', this.audioChannel);
+        },
+
+        toggleAudio: function(event) {
+            if (event) event.preventDefault();
+ 
+            if ($(event.currentTarget).hasClass('playing')) {
+                Adapt.trigger('audio:pauseAudio', this.audioChannel);
+            } else {
+                Adapt.trigger('audio:playAudio', this.audioFile, this.elementId, this.audioChannel);
+            }
+        }
 
     });
 
-    /////////////////////////////////
+    Adapt.on('router:menu', function(model) {
+        $('#wrapper').append(new BoxMenuView({model: model}).$el);
+    });
     
     Adapt.on('menuView:postRender', function(view) {
         
@@ -131,7 +143,9 @@ define([
         }
         
     });
-    
-    /////////////////////////////////
+
+    Adapt.once('router:menu', function(pageModel) {
+        new BoxMenuAudioView({model:pageModel});
+    });
 
 });
